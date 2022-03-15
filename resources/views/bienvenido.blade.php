853e268eb845cca1beed458416743b0ab8f3775b
francisco ramirez <gforce3002@gmail.com>
2022-03-15 15:55:28 -0600

    Creacion de controladores,  aplicaciones de rutas y creacion de modelos

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+</head>
+<body>
+    <h1>Bienvenido</h1>
+    <a href="/contacto">Contacto</a>
+    <!-- ruta con nombre 
+        Es mejor usar la ruta con nombre ya que si se cambia el dato
+    de la ruta es mas facil realizar un referenciado y no tener que cambiar todo
+    el codigo del sistema 
+    ejemplo "/contacto" => "/llamame"
+    esta ruta se cambio en el archivo de rutas y seria cambiar en todo el sistema 
+    
+    Ahora con las rutas con nombre eso ya no pasaria se cambia la uri pero con el
+    nombre de la ruta podemos acceder sin cambiar ni una linea de codigo
+-->
+    <a href="{{route('contacto')}}">Contacto (ruta con nombre)</a>
+</body>
+</html>
---
+++ resources/views/custom.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customs laravel 9</title>
+</head>
+<body>
+    <h1>Esta es la vista Customs</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus error tenetur sit facilis illum quibusdam illo sunt! Iure saepe adipisci accusamus nisi? Itaque quibusdam natus laudantium, ab enim repellat quae!</p>
+    <?php echo $msj ?>
+    <?= $msj ?>
+    {{$msj}} <!-- formato en blaze -->
+
+    {{$flag}}
+</body>
+</html>
